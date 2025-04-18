49422f96eb8ac4ade502d9590e0368d812e404f9
Lotte Echholt <lotteoe@stud.ntnu.no>
2025-04-18 21:32:48 +0200

    reset prosject before implementing gui

---
+++ app/backend/models/songs.py
@@ -0,0 +1,15 @@
+from sqlalchemy import Column, Integer, String, Date, ForeignKey
+from sqlalchemy.orm import relationship
+from app.backend.db import Base
+
+class Song(Base):
+    __tablename__ = 'songs'
+
+    id = Column(Integer, primary_key=True)
+    title = Column(String, nullable=False)
+    duration = Column(Integer, nullable=False)
+    genre = Column(String)
+    release_date = Column(Date)
+    artist_id = Column(Integer, ForeignKey('artists.id'))
+
+    artist = relationship("Artist", back_populates="songs")
